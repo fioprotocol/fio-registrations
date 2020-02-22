64b28922409cddd10ccc101ed9fe4df2688c3511
james <james@librem.slim>
2020-02-22 12:06:18 -0600

    Add /public-api/summary to apidoc

--- server/routes/account.js
+++ server/routes/account.js
@@ -30,7 +30,77 @@ router.get('/public-api/balance/:publicKey', hourlyLimit(20), handler(async (req
   return res.send({success: true, balance})
 }))
 
-/** Transaction Monitor.  TrxMonitor.vue */
+// @see Transaction Monitor.  TrxMonitor.vue
+/**
+  @api {post} /public-api/summary summary
+  @apiGroup Registration
+  @apiName Summary
+
+  @apiDescription
+  Check the status of a account or domain registration
+  @apiParamExample {json} POST-Example:
+  HTTP/1.1 200 OK
+  {
+    publicKey: String, // Full FIO public key (like: FIO5fnv..DZSYu)
+  }
+
+  .. or ..
+
+  HTTP/1.1 200 OK
+  {
+    address: String, // before the '@'
+    domain: String // after the '@'
+  }
+
+  @apiSuccessExample Success-Response
+  [
+    {
+      address: null,
+      domain: 'fio',
+      owner_key: 'FIO5fnvQGmLRv5JLytqgvfWZfPyi4ousY46zdRU9MSSzJksFDZSYu',
+      trx_type: 'register',
+      trx_id: '1fb666a590d8d6a0334b0ad7147c8eb65a83de8932dbc75bbcf158c0ecfaaa23',
+      expiration: '2020-02-22T17:27:33.500Z',
+      block_num: 1531052,
+      'BlockchainTrxEvents.id': 5,
+      trx_status: 'success',
+      trx_status_notes: 'irreversible',
+      pay_source: 'free',
+      forward_url: null,
+      buy_price: '0.03',
+      pay_metadata: null,
+      extern_id: null,
+      'AccountPayEvents.id': 9,
+      pay_status: 'success',
+      pay_status_notes: null,
+      extern_time: null,
+      extern_status: null
+    },
+    {
+      address: 'memeber',
+      domain: 'fio',
+      owner_key: 'FIO5fnvQGmLRv5JLytqgvfWZfPyi4ousY46zdRU9MSSzJksFDZSYu',
+      trx_type: 'register',
+      trx_id: '4cdb133aa17a2bde2fd4b4fca05b6d6737397ab8a3906b51f7f6dd3376bf4316',
+      expiration: '2020-02-22T15:15:52.500Z',
+      block_num: null,
+      'BlockchainTrxEvents.id': 2,
+      trx_status: 'review',
+      trx_status_notes: 'FIO Domain is not public. Only owner can create FIO Addresses.',
+      pay_source: 'free',
+      forward_url: null,
+      buy_price: '0.03',
+      pay_metadata: null,
+      extern_id: null,
+      'AccountPayEvents.id': 8,
+      pay_status: 'success',
+      pay_status_notes: null,
+      extern_time: null,
+      extern_status: null
+    },
+    ...
+  ]
+*/
 router.post('/public-api/summary', handler(async (req, res) => {
   const {publicKey} = req.body
   let {referralCode, trxStatus} = req.body
